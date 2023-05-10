ba883a36b7dd572e90c780ce3a36d3ba156bdbeb
E Luckie <eluckie.d@gmail.com>
2023-05-10 11:55:40 -0600

    WIP: add firebase authentication

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,35 @@
 import { Link } from "react-router-dom";
+import { getAuth } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 function SignIn() {
+
+  function handleSignIn(e) {
+    e.preventDefault();
+    const username = e.target.username.value.toLowerCase();
+    const password = e.target.password.value;
+
+  }
+
   return (
     <>
-      <h5>log in placeholder</h5>
-      <Link to="/"><button>cancel</button></Link>
+      <br/>
+      <hr/>
+      <br/>
+      <form id="sign-in" onSubmit={handleSignIn}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"/>
+        <br/>
+        <input
+          type="password"
+          name="password"
+          placeholder="password"/>
+        <br/><br/>
+        <button type="submit">go</button>
+      </form>
+      <Link to="/"><p>cancel</p></Link>
     </>
   );
 }
